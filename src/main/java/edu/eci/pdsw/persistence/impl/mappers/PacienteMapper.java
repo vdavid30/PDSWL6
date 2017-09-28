c114094c21e7f2c40b69ab36069257492609603d
Héctor Cadavid <hector.cadavid@gmail.com>
2017-09-28 09:55:46 -0500

    first commit

--- src/main/java/edu/eci/pdsw/persistence/impl/mappers/PacienteMapper.java
+++ src/main/java/edu/eci/pdsw/persistence/impl/mappers/PacienteMapper.java
@@ -2,6 +2,7 @@ package edu.eci.pdsw.persistence.impl.mappers;
 
 
 
+import edu.eci.pdsw.samples.entities.Consulta;
 import edu.eci.pdsw.samples.entities.Paciente;
 import java.util.Date;
 
@@ -16,5 +17,9 @@ public interface PacienteMapper {
     public Paciente loadPacienteById(int id,String tipoid);
     
     public List<Paciente> loadPacientes();
+    
+    public void insertarPaciente(Paciente p);
+    
+    public void insertConsulta(Consulta con,int idPaciente, String tipoid,int costoconsulta);
 
 }
